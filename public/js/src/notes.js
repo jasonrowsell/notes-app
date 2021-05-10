616235818e0f7b5b4afc44561de0a5bf13cbd795
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-10 15:49:10 +0100

    Skeleton implemented

--- public/js/src/notes.js
+++ public/js/src/notes.js
@@ -1,5 +1,17 @@
 class Notes {
+  constructor() {
+    this.notes = [];
+  }
+
   abbreviate(string) {
     return string.substr(0, 20) + '...';
+  }
+
+  createNote(string) {
+    this.notes.push(string);
+  }
+
+  showNotes() {
+    return this.notes;
   }
 }
