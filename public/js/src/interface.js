b703a5427016a3c1dac9d16db04c5ddb66a9c726
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-11 22:19:30 +0100

    Updated local storage

--- public/js/src/interface.js
+++ public/js/src/interface.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   localStorage.setItem('notes', JSON.stringify(notesArray));
   const data = JSON.parse(localStorage.getItem('notes'));
 
-  const liMaker = (text) => {
+  const listMaker = (text) => {
     const li = document.createElement('li');
     li.textContent = text;
     ul.appendChild(li);
@@ -21,46 +21,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     notesArray.push(noteText.value);
     localStorage.setItem('notes', JSON.stringify(notesArray));
-    liMaker(noteText.value);
+    listMaker(noteText.value);
     noteText.value = '';
   });
 
   data.forEach((item) => {
-    liMaker(item);
+    note = new Note(item.text);
+    listMaker(note.abbreviate());
   });
-
-  // localStorage.setItem('notes', JSON.stringify(notesArray));
-  // const data = JSON.parse(localStorage.getItem('notes'));
-
-  // submits and displays note
-
-  // noteForm.addEventListener('submit', (event) => {
-  //   event.preventDefault();
-  //   createNote(event);
-  //   displayNote();
-  //   localStorage.setItem('notes', JSON.stringify(notesArray));
-  //   const data = JSON.parse(localStorage.getItem('notes'));
-  // });
-
-  // function createNote(event) {
-  //   event.preventDefault();
-  //   const text = noteText.value;
-  //   note = new Note(text);
-  //   notesArray.push(note);
-  //   localStorage.setItem('notes', JSON.stringify(notesArray));
-  // }
-
-  // function displayNote() {
-  //   data.forEach((note) => {
-  //     _listMaker(note.text);
-  //   });
-  // }
-
-  // function _listMaker(note) {
-  //   const p = document.createElement('p');
-  //   p.innerHTML = note;
-  //   document.body.appendChild(p);
-  // }
 
   // textarea auto resize
 
