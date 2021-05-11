3d5a398a1c87bf030dad715b19cb2ffe0121fe64
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-11 22:06:30 +0100

    Local storage set up without note object

--- public/js/src/interface.js
+++ public/js/src/interface.js
@@ -1,22 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
   let noteForm = document.querySelector('#note-form'),
+    ul = document.querySelector('ul'),
     noteText = document.querySelector('#note-text'),
-    notes = document.querySelector('#notes');
+    notes = document.querySelector('#notes'),
+    notesArray = localStorage.getItem('notes')
+      ? JSON.parse(localStorage.getItem('notes'))
+      : [];
 
-  noteForm.addEventListener('submit', (event) => {
-    createNote(event);
-    displayNote();
+  localStorage.setItem('notes', JSON.stringify(notesArray));
+  const data = JSON.parse(localStorage.getItem('notes'));
+
+  const liMaker = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+  };
+
+  noteForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    notesArray.push(noteText.value);
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+    liMaker(noteText.value);
+    noteText.value = '';
   });
 
-  function createNote(event) {
-    event.preventDefault();
-    const text = noteText.value;
-    note = new Note(text);
-  }
+  data.forEach((item) => {
+    liMaker(item);
+  });
 
-  function displayNote() {
-    notes.innerText = note.abbreviate();
-  }
+  // localStorage.setItem('notes', JSON.stringify(notesArray));
+  // const data = JSON.parse(localStorage.getItem('notes'));
+
+  // submits and displays note
+
+  // noteForm.addEventListener('submit', (event) => {
+  //   event.preventDefault();
+  //   createNote(event);
+  //   displayNote();
+  //   localStorage.setItem('notes', JSON.stringify(notesArray));
+  //   const data = JSON.parse(localStorage.getItem('notes'));
+  // });
+
+  // function createNote(event) {
+  //   event.preventDefault();
+  //   const text = noteText.value;
+  //   note = new Note(text);
+  //   notesArray.push(note);
+  //   localStorage.setItem('notes', JSON.stringify(notesArray));
+  // }
+
+  // function displayNote() {
+  //   data.forEach((note) => {
+  //     _listMaker(note.text);
+  //   });
+  // }
+
+  // function _listMaker(note) {
+  //   const p = document.createElement('p');
+  //   p.innerHTML = note;
+  //   document.body.appendChild(p);
+  // }
 
   // textarea auto resize
 
