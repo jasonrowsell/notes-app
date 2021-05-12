8eed8a672c18a89b0b195471ccc4ceb4ad2384c9
Toby Jessup <toby4444@hotmail.com>
2021-05-12 16:00:43 +0100

    Second test passes

--- public/js/spec/noteSpec.js
+++ public/js/spec/noteSpec.js
@@ -1,7 +1,11 @@
 'use strict'
 
-let note = new Note("note");
+let note = new Note("noteislongerthan20characters");
 
 it('returns the text', function () {
-  expect(note.getNote()).toEqual("note"); 
+  expect(note.getNote()).toEqual("noteislongerthan20characters"); 
+})
+
+it ('returns the text abbreviate', function () {
+  expect(note.abbreviate()).toEqual("noteislongerthan20ch...")
 })
--- public/js/spec/testingSpec.js
+++ public/js/spec/testingSpec.js
@@ -17,11 +17,3 @@ function it(label, callback) {
   callback()
 }
 
-it('2 + 2 is equal to 4', function(){
-  expect(2 + 2).toEqual(4)   
-})
-
-it('2 + 3 is equal to 4', function(){
-    expect(2 + 3).toEqual(4)   
-})
-
